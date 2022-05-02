ff48f73ccb279c3e98f535ee7c5834c96c05a659
Brendson <brendson.net@gmail.com>
2022-05-02 12:08:52 -0300

    feat: created Components styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import GlobalStyles from './styles/GlobalStyles'
+
+import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout';
 
 class App extends Component {
   render() {
     return (
-      <>
       <div>
-          <h1>hello world</h1>
-      </div>
-
+      <Layout />
       <GlobalStyles />
-      </>
+      </div>
     );
   }
 }
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../Header';
+
+import { Container } from './styles';
+
+export default function Layout() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
