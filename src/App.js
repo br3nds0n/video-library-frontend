0c096d62025347551938215e6bacb2b8dcec9e77
Brendson <brendson.net@gmail.com>
2022-05-02 10:57:03 -0300

    feat: created Global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import GlobalStyles from './styles/GlobalStyles'
 
 class App extends Component {
   render() {
     return (
+      <>
       <div>
-        <h1>hello world</h1>
+          <h1>hello world</h1>
       </div>
+
+      <GlobalStyles />
+      </>
     );
   }
 }
