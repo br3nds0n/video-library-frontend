9dcc17d462a51e435a777312bb540edfdc62f812
Brendson <brendson.net@gmail.com>
2022-05-06 11:32:46 -0300

    feat: created context and axios api

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import Layout from './components/Layout';
 
+import { VideoContextProvider } from './context/VideoContext'
+
 class App extends Component {
   render() {
     return (
       <>
-      <Layout />
+      <VideoContextProvider> 
+       <Layout />
+      </VideoContextProvider>
       <GlobalStyles />
       </>
     );
