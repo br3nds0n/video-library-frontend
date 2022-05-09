f1dc50d460491c3a45d78291241a304e53e5a41f
Brendson <brendson.net@gmail.com>
2022-05-09 14:19:14 -0300

    feat: created Component FormModal

--- src/components/AddVideo/index.jsx
+++ src/components/AddVideo/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { VideoContext } from '../../context/VideoContext';
 
 import { AddVideoButton, AddIcon } from './styles';
 
 export default function AddVideo() {
+  const { handleAdd } = useContext(VideoContext);
+
   return (
     <li>
-      <AddVideoButton>
+      <AddVideoButton onClick={handleAdd}>
         <AddIcon />
       </AddVideoButton>
     </li>
